82294093ee532f76c38158f903a81750f76950b8
sidrug21 <sidrug.beth21@gmail.com>
2023-03-10 01:55:53 +0900

    Issue4~6の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,11 +12,22 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to calendars_path
+    else
+      render :new
+    end
+  end
+  
+
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
+  
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
@@ -34,7 +45,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[(@todays_date + x).wday]}
       @week_days.push(days)
     end
 
